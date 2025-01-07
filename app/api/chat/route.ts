7810c479d026cfe7a26845210ad33435c091d6aa
Aman Kumar Singh Kushwaha <amansinghkushwaha2003@gmail.com>
2025-01-07 20:39:13 +0530

    🛠️ Vercel SDK Tools support for addResource

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,8 @@
+import { createResource } from '@/lib/actions/resources';
+
 import { openai } from '@ai-sdk/openai';
-import { streamText } from 'ai';
+import { streamText, tool } from 'ai';
+import { z } from 'zod';
 
 //Response stream upto 30s
 export const maxDuration = 30;
@@ -13,6 +16,18 @@ export async function POST(req:Request) {
     Only respond to questions using information from tool calls.
     if no relevant information is found in the tool calls, respond humbly, "Sorry, I don't know."`,
     messages,
+    tools:{
+      addResource: tool({
+        description:  `add a resource to your knowledge base.
+        If the user provides a random piece of knowledge unprompted, use this tool without asking for confirmation.`,
+      parameters: z.object({
+        content: z
+          .string()
+          .describe('the content or resource to add to the knowledge base'),
+      }),
+      execute: async ({content}) => createResource({content}),
+      }),
+    },
   })
 
   return result.toDataStreamResponse();
